bb4b67c378efc20fc4134c9d19e684fa1ebfcac5
Daniel2193 <daniel2193mc@gmail.com>
2021-12-24 15:19:04 +0100

    test1

---
+++ Program.cs
@@ -0,0 +1,429 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EmoteDownloader
+{
+    class Program
+    {
+        private static string version = "1.0.1";
+        public static async Task<int> Main(params string[] args)
+        {
+            RootCommand rootCommand = new RootCommand(
+                description: "Bulk download emotes from Twitch/BTTV/FFZ/7TV"
+            );
+            Option platform = new Option<string?>(
+                aliases: new string[] { "-p", "--platform" },
+                description: "Platform to download from. Valid values: twitch, bttv, ffz, 7tv"
+            );
+            Option client_id = new Option<string?>(
+                aliases: new string[] { "-cid", "--client-id" },
+                description: "Client ID, not required if token is provided or platform is not twitch and channel_ids is provided"
+            );
+            Option client_secret = new Option<string?>(
+                aliases: new string[] { "-cs", "--client-secret" },
+                description: "Client Secret, not required if token is provided or platform is not twitch and channel_ids is provided"
+            );
+            Option token = new Option<string?>(
+                aliases: new string[] { "-t", "--token" },
+                description: "Token, not required if client ID and secret are provided or platform is not twitch and channel_ids is provided"
+            );
+            Option channel_ids = new Option<string?>(
+                aliases: new string[] { "-ci", "--channel-ids" },
+                description: "Channel IDs, separated by commas. Not required if channel names are provided but"
+            );
+            Option channel_names = new Option<string?>(
+                aliases: new string[] { "-cn", "--channel-names" },
+                description: "Channel Names, separated by commas. Not required if channel IDs are provided"
+            );
+            Option output_dir = new Option<string?>(
+                defaultValue: Path.Combine(Directory.GetCurrentDirectory(), "emotes"),
+                aliases: new string[] { "-o", "--output-dir" },
+                description: "Output directory, will be created if it doesn't exist and defaults to current directory if not provided"
+            );
+            Option version = new Option<bool>(
+                defaultValue: false,
+                aliases: new string[] { "-v", "--version" },
+                description: "Print version"
+            );
+            Option help = new Option<bool>(
+                defaultValue: false,
+                aliases: new string[] { "-?", "-h", "--help" },
+                description: "Print help"
+            );
+            Option verbose = new Option<bool>(
+                defaultValue: false,
+                aliases: new string[] { "--verbose" },
+                description: "Enables verbose output, intended for debugging purposes"
+            );
+            rootCommand.AddOption(platform);
+            rootCommand.AddOption(client_id);
+            rootCommand.AddOption(client_secret);
+            rootCommand.AddOption(token);
+            rootCommand.AddOption(channel_ids);
+            rootCommand.AddOption(channel_names);
+            rootCommand.AddOption(output_dir);
+            rootCommand.AddOption(version);
+            rootCommand.AddOption(help);
+            rootCommand.AddOption(verbose);
+
+            Action<string?, string?, string?, string?, string?, string?, string?, bool, bool, bool, IConsole> downloadEmotes = async (platform, client_id, client_secret, token, channel_ids, channel_names, output_dir, verbose, help, version, console) =>
+            {
+                #region Validation
+                bool useNames = false;
+                bool tokenRequired = true;
+                if (help)
+                {
+                    rootCommand.Invoke(new string[] { "--help" });
+                    return;
+                }
+                if (version)
+                {
+                    console.Out.Write($"\nEmoteDownloader v{Program.version}");
+                    return;
+                }
+                if (verbose)
+                {
+                    console.Out.Write("Enabled verbose output\n");
+                    //write all parameters to console
+                    console.Out.Write($"Platform: {platform} <-- Is Null {platform == null}\n");
+                    console.Out.Write($"Client ID: {client_id} <-- Is Null {client_id == null}\n");
+                    console.Out.Write($"Client Secret: {client_secret} <-- Is Null {client_secret == null}\n");
+                    console.Out.Write($"Token: {token} <-- Is Null {token == null}\n");
+                    console.Out.Write($"Channel IDs: {channel_ids} <-- Is Null {channel_ids == null}\n");
+                    console.Out.Write($"Channel Names: {channel_names} <-- Is Null {channel_names == null}\n");
+                    console.Out.Write($"Output Directory: {output_dir} <-- Is Null {output_dir == null}\n");
+                    console.Out.Write($"Verbose: {verbose} <-- Is Null {verbose == null}\n");
+                    console.Out.Write($"Help: {help} <-- Is Null {help == null}\n");
+                    console.Out.Write($"Version: {version} <-- Is Null {version == null}\n");
+                }
+                if (platform == null)
+                {
+                    console.Error.Write("Platform is required\n");
+                    return;
+                }
+                else if (platform.ToLower() != "twitch" && platform.ToLower() != "bttv" && platform.ToLower() != "ffz" && platform.ToLower() != "7tv")
+                {
+                    console.Error.Write($"Invalid platform: {platform}\n");
+                    return;
+                }
+                if ((platform.ToLower() == "twitch" || channel_ids == null) && ((client_id == null || client_secret == null) && token == null))
+                {
+                    console.Error.Write("Client ID and secret or token is required for Twitch or when using channel names\n");
+                    return;
+                }
+                if (channel_ids == null && channel_names == null)
+                {
+                    console.Error.Write("Channel IDs or channel names is required\n");
+                    return;
+                }
+                if (channel_ids != null && channel_names != null)
+                {
+                    if (verbose)
+                    {
+                        console.Out.Write("Channel IDs and channel names provided, using channel IDs\n");
+                    }
+                    useNames = false;
+                }
+                if (channel_ids != null && channel_names == null)
+                {
+                    if (verbose)
+                    {
+                        console.Out.Write("Channel IDs provided, using channel IDs\n");
+                    }
+                    useNames = false;
+                }
+                if (channel_names != null && channel_ids == null)
+                {
+                    if (verbose)
+                    {
+                        console.Out.Write("Channel names provided, using channel names\n");
+                    }
+                    useNames = true;
+                }
+                if (channel_ids != null && channel_ids.Split(',').Length == 0)
+                {
+                    console.Error.Write("Channel IDs cannot be empty\n");
+                    return;
+                }
+                if (channel_names != null && channel_names.Split(',').Length == 0)
+                {
+                    console.Error.Write("Channel names cannot be empty\n");
+                    return;
+                }
+                if (output_dir == null)
+                {
+                    if (verbose)
+                    {
+                        console.Out.Write("Output directory not provided, using current directory\n");
+                    }
+                }
+                else if (output_dir.Equals(""))
+                {
+                    output_dir = Path.Combine(Directory.GetCurrentDirectory(), "emotes");
+                }
+                else if (!Directory.Exists(output_dir))
+                {
+                    if (verbose)
+                    {
+                        console.Out.Write($"Output directory does not exist, creating: {output_dir}\n");
+                    }
+                    Directory.CreateDirectory(output_dir);
+                }
+                tokenRequired = (platform.ToLower() == "twitch" || useNames);
+                #endregion
+                #region preparing
+                if (verbose)
+                {
+                    console.Out.Write("Preparing...\n");
+                }
+                List<string> channel_ids_list = new List<string>();
+                if (tokenRequired && token == null)
+                {
+                    if (verbose)
+                    {
+                        console.Out.Write("Token not provided, using client ID and secret to get a token\n");
+                    }
+                    token = await TwitchGetToken(client_id, client_secret);
+                    if (token == null)
+                    {
+                        console.Error.Write("Failed to get token\nPlease ensure that client ID and secret are correct\n");
+                        return;
+                    }
+                    if (verbose)
+                    {
+                        console.Out.Write($"Got Token: {token}\n");
+                    }
+                }
+                if (useNames)
+                {
+                    if (verbose)
+                    {
+                        console.Out.Write($"Getting channel IDs from channel names\n");
+                    }
+                    string url = "https://api.twitch.tv/helix/users?login=" + channel_names.Replace(",", "&login=");
+                    string userJson = await GetApiJson(url, token, client_id, true);
+                    if (userJson == null)
+                    {
+                        console.Error.Write("Failed to get channel IDs due to an API error\n");
+                        return;
+                    }
+                    if (verbose)
+                    {
+                        console.Out.Write($"Got channel IDs from channel names\n");
+                    }
+                    var user = JObject.Parse(userJson);
+                    foreach (var user_id in user["data"])
+                    {
+                        channel_ids_list.Add(user_id["id"].ToString());
+                    }
+                }
+                else
+                {
+                    channel_ids_list.AddRange(channel_ids.Split(','));
+                }
+                if (verbose)
+                {
+                    console.Out.Write($"Got {channel_ids_list.Count} channel IDs\n");
+                }
+                #endregion
+                #region Getting emotes
+
+
+
+                //TO DO: Add support for animated emotes (v1.1.0)
+                //Extend Dictionary to <Emote, bool> to indicate if it's animated
+
+
+
+                if (verbose)
+                {
+                    console.Out.Write("Getting emotes...\n");
+                }
+                Dictionary<string, string> emotes = new Dictionary<string, string>();
+                foreach (var channel_id in channel_ids_list)
+                {
+                    if (verbose)
+                    {
+                        console.Out.Write($"Getting emotes for channel ID: {channel_id}\n");
+                    }
+                    string url = "";
+                    if (platform.ToLower() == "twitch")
+                    {
+                        url = $"https://api.twitch.tv/helix/chat/emotes?broadcaster_id={channel_id}";
+                    }
+                    else if (platform.ToLower() == "bttv")
+                    {
+                        url = $"https://api.betterttv.net/3/cached/users/twitch/{channel_id}";
+                    }
+                    else if (platform.ToLower() == "ffz")
+                    {
+                        url = $"https://api.betterttv.net/3/cached/frankerfacez/users/twitch/{channel_id}";
+                    }
+                    else if (platform.ToLower() == "7tv")
+                    {
+                        url = $"https://api.7tv.app/v2/users/{channel_id}/emotes";
+                    }
+                    string emotesJson = await GetApiJson(url, token, client_id);
+                    if (emotesJson == null)
+                    {
+                        console.Error.Write("Failed to get emotes due to an API error\n");
+                        return;
+                    }
+                    if (verbose)
+                    {
+                        console.Out.Write($"Got emotes for channel ID: {channel_id}\n");
+                    }
+                    JObject emotesObj;
+                    if (platform.ToLower() == "ffz" || platform.ToLower() == "7tv")
+                    {
+                        emotesObj = JObject.Parse("{\"data\": " + emotesJson + "}");
+                    }
+                    else
+                    {
+                        emotesObj = JObject.Parse(emotesJson);
+                    }
+                    if (platform.ToLower() == "bttv")
+                    {
+                        if (emotesObj["channelEmotes"] != null)
+                        {
+                            foreach (var emote in emotesObj["channelEmotes"])
+                            {
+                                emotes.Add(emote["code"].ToString(), bttvLink(emote["id"].ToString()));
+                            }
+                        }
+                        if (emotesObj["sharedEmotes"] != null)
+                        {
+                            foreach (var emote in emotesObj["sharedEmotes"])
+                            {
+                                emotes.Add(emote["code"].ToString(), bttvLink(emote["id"].ToString()));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach (var emote in emotesObj["data"])
+                        {
+                            string link = "";
+                            if (platform.ToLower() == "twitch")
+                            {
+                                if (emote["images"]["url_4x"] != null)
+                                {
+                                    link = emote["images"]["url_4x"].ToString();
+                                }
+                                else if (emote["images"]["url_2x"] != null)
+                                {
+                                    link = emote["images"]["url_2x"].ToString();
+                                }
+                                else if (emote["images"]["url_1x"] != null)
+                                {
+                                    link = emote["images"]["url_1x"].ToString();
+                                }
+                                else
+                                {
+                                    if(verbose)
+                                    {
+                                        console.Out.Write($"Unable get url for {emote["code"].ToString()}\n");
+                                    }
+                                    continue;
+                                }
+                            }
+                            else if(platform.ToLower() == "7tv"){
+                                if(emote["urls"] != null){
+                                    foreach(var item in emote["urls"]){
+                                        if(item[1] != null){
+                                            link = item[1].ToString();
+                                        }
+                                    }
+                                }
+                            }
+                            else if(platform.ToLower() == "ffz"){
+                                link = $"https://cdn.betterttv.net/frankerfacez_emote/{emote["id"]}/4";
+                            }
+                            emotes.Add(emote["name"].ToString(), link);
+                        }
+                    }
+                }
+                #endregion
+                console.Out.Write("Download started\n");
+                Task.WaitAll(downloadEmotesAsync(emotes, output_dir).ToArray());
+                console.Out.Write("Download complete!\n");
+            };
+            rootCommand.Handler = CommandHandler.Create(downloadEmotes);
+            return await rootCommand.InvokeAsync(args);
+        }
+        private static string bttvLink(string id)
+        {
+            return $"https://cdn.betterttv.net/emote/{id}/3x";
+        }
+        private static async Task<string> TwitchGetToken(string client_id, string client_secret)
+        {
+            using (var request = new HttpRequestMessage(HttpMethod.Post, "https://id.twitch.tv/oauth2/token?client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials"))
+            {
+                using (var client = new HttpClient())
+                {
+                    using (var response = await client.SendAsync(request))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var json = await response.Content.ReadAsStringAsync();
+                            var obj = JObject.Parse(json);
+                            if (obj["access_token"] != null)
+                            {
+                                return obj["access_token"].ToString();
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+        private static async Task<string> GetApiJson(string url, string token, string clientID, bool twitch = false)
+        {
+            using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+            {
+                if (twitch)
+                {
+                    request.Headers.Add("Authorization", "Bearer " + token);
+                    request.Headers.Add("Client-Id", clientID);
+                }
+                using (var client = new HttpClient())
+                {
+                    using (var response = await client.SendAsync(request))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            return await response.Content.ReadAsStringAsync();
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+        private static List<Task> downloadEmotesAsync(Dictionary<string, string> d, string output){
+            List<Task> taskList = new List<Task>();
+            foreach (var item in d)
+            {
+                WebClient wc = new WebClient();
+                taskList.Add(wc.DownloadFileTaskAsync(item.Key, Path.Combine(output, item.Value + ".png")));
+            }
+            return taskList;
+        }
+    }
+}
